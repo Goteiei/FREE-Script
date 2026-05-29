a9d0c40bab2d41a5b1f226d6a06e19ac1eeacd61
Godzila <singhathongj@gmail.com>
2026-05-30 06:28:17 +0700

    Update Lua script download URL

--- .lua
+++ .lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3e135b5db4020f532fa033f0b8fb059992240949b2c15c4d43c3aeb09a0ae543/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e79c2648703ff098640eafce7c68706188b884e7ecea7a57c52b1a30f7bea83e/download"))()
